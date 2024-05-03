2302ed515285c17edea3867692b7ce917328f417
alpaka206 <gyuwon05@gmail.com>
2024-05-03 17:10:31 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "framework-de157c6b30d4790e4e81.js"
   },
   {
-    "url": "app-9b4b16d94f2d828fb61d.js"
+    "url": "app-8681cefd151f6d6df4ee.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "851286e67227738369f2dc25b553fd08"
+    "revision": "ee5854e7b6b6fce5f3f1cbe1d7daa435"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -166,7 +166,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-9b4b16d94f2d828fb61d.js`))) {
+  if (!resources || !(await caches.match(`/app-8681cefd151f6d6df4ee.js`))) {
     return await fetch(event.request)
   }
 
