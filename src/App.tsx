e13428ef45023a63af484f7e17e7713e65c8d446
alpaka206 <gyuwon05@gmail.com>
2024-05-31 15:04:02 +0900

    라우팅"

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from "react";
+import { RecoilRoot } from "recoil";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Mainpage from "./pages/Mainpage.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+// import "./axiosConfig.jsx";
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <RecoilRoot>
+      <div className="App">
+        {/* <OpenExternalBrowser /> */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Mainpage />} />
+          </Routes>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </RecoilRoot>
+  );
 }
-
-export default App
---
+++ src/pages/Mainpage.tsx
@@ -0,0 +1,6 @@
+function Mainpage() {
+  // 로그인 상태에 따라 다른 컴포넌트 렌더링
+  return <div>main</div>;
+}
+
+export default Mainpage;
