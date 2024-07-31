e1c2aacfa3c8022a81186c3e2c56a914cebf5580
alpaka206 <gyuwon05@gmail.com>
2024-07-31 14:15:55 +0900

    retest

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,11 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
   build: {
-    sourcemap: false, // 소스 맵을 비활성화합니다.
-    // rollupOptions: {
-    //   output: {
-    //     format: "iife", // ES 모듈 대신 IIFE 형식을 사용합니다.
-    //   },
-    // },
+    sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: undefined, // 모든 코드가 하나의 번들로 합쳐지도록 설정
+        manualChunks: undefined,
       },
     },
   },
-  // base: "/",
 });
