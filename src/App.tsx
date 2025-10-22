6b4056637fe35230dd6d51a6c3ca4b3195c7ffae
alpaka206 <gyuwon05@gmail.com>
2025-10-22 17:31:28 +0900

    feat: 첫 페이지 화면 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import { useExternalBrowserRedirect } from './hooks/useExternalBrowserRedirect';
 import AppRoutes from './routes/AppRoutes.tsx';
+import FullscreenGate from '@/components/FullscreenGate/FullscreenGate.tsx';
 
 export default function App() {
   useExternalBrowserRedirect();
 
   return (
     <BrowserRouter>
+      <FullscreenGate />
       <AppRoutes />
     </BrowserRouter>
   );
